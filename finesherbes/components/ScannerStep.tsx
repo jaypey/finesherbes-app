69194c191863cbb16ffac86fa54f78c5be821387
JP Belval <jpbelv@gmail.com>
2023-11-13 15:59:02 -0500

    Add stepper logic

--- finesherbes/components/ScannerStep.tsx
+++ finesherbes/components/ScannerStep.tsx
@@ -4,13 +4,40 @@ import { SegmentedButtons, Text } from 'react-native-paper';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const ScannerStep = () => {
-  const [value, setValue] = React.useState('');
+
+  const readCode = (readTxt: string) => {
+    console.log(readTxt);
+
+    switch (readTxt) {
+      case "1":
+        
+        break;
+      case "2":
+        
+        break;
+      case "3":
+        
+        break;       
+      case "4":
+      
+      break;
+      case "5":
+        
+        break;
+      case "6":
+        
+        break;     
+      default:
+        console.error("Invalid code!");
+        break;
+    }
+  }
 
   return (
     <View style={styles.container}>
         <View style={styles.scannerInput}>
             <Text variant="displaySmall">Scanner</Text>
-            <QRCodeScanner onRead={(e) => {console.error(e.data)}} />
+            <QRCodeScanner onRead={(e) => readCode(e.data)} />
         </View>
     </View>
   );
--- finesherbes/components/Stepper.tsx
+++ finesherbes/components/Stepper.tsx
@@ -1,31 +1,58 @@
 import * as React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { SegmentedButtons } from 'react-native-paper';
+import ScannerStep from './ScannerStep';
+
+type Step = {
+  stepName: string
+  stepDisplayName: string
+  isCurrent: boolean
+}
+
+const updateSteps = (value: string, steps: Step[]) => {
+  return steps.map((e) => {
+    if(e.stepName == value){
+      e.isCurrent = true;
+      return e;
+    }
+    e.isCurrent = false;
+    return e;
+  });
+}
 
 const Stepper = () => {
-  const [value, setValue] = React.useState('');
+
+  const [steps, setSteps] = React.useState<Step[]>([
+    {
+    stepName: "scan",
+    stepDisplayName: "Scanner",
+    isCurrent: true
+    },
+    {
+      stepName: "order",
+      stepDisplayName: "Commander",
+      isCurrent: false
+    },
+    {
+      stepName: "eat",
+      stepDisplayName: "Savourer",
+      isCurrent: false
+    },
+]);
 
   return (
-    <SegmentedButtons
-    value={value}
-    onValueChange={setValue}
-    buttons={[
-        {
-        value: 'walk',
-        label: 'Scanner',
-        },
-        {
-        value: 'train',
-        label: 'Commander',
-        disabled: true
-        },
-        { 
-        value: 'drive',
-        label: 'Savourer',
-        disabled: true
-        },
-    ]}
-    />
+    <>
+      <SegmentedButtons
+      value={steps.find(x=>x.isCurrent)!.stepName}
+      onValueChange={(v) => {setSteps(updateSteps(v, steps))}}
+      buttons={
+        steps.map((e) => {
+          return { value: e.stepName, label: e.stepDisplayName, disabled: !e.isCurrent}
+        })
+      }
+      />
+      <ScannerStep></ScannerStep>
+    </>
   );
 };
 
